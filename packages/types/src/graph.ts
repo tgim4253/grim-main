29b9ca5c335636af128413d5abb01e67cdcc4fd4
tgim4253 <tgim4253@gmail.com>
2025-08-10 13:49:05 +0900

    create graph types

---
+++ packages/types/src/graph.ts
@@ -0,0 +1,36 @@
+export interface GraphResponse {
+  nodes: Node[];
+  connections: Connection[];
+}
+
+export interface Connection {
+  id: string;
+  src_node_id: string;
+  dst_node_id: string;
+  kind_rule_id: string;
+  kind: string;
+  weight: number;
+}
+
+export interface Node {
+  id: string;
+  kind: string;
+  data: NodeData;
+  created_at: string;
+  updated_at: string;
+}
+
+export type NodeData = NodeFolder | NodeFile;
+
+export interface NodeFolder {
+  folder_id: string;
+  node_id: string;
+  real_folder_id: string | null;
+  folder_name: string | null;
+}
+
+export interface NodeFile {
+  file_id: string;
+  node_id: string;
+  sha256: string | null;
+}
--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,2 +1,3 @@
 export * from './tree';
 export * from './panel';
+export * from './graph';
